8bb71bd7828ecca493cebefcf7066ab2f919fc6c
Chetram Bassit <chetrambassit121@gmail.com>
2023-07-14 12:04:29 -0400

    static and media files setup

--- WeatherApp/settings.py
+++ WeatherApp/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 import environ
 env = environ.Env()
 environ.Env.read_env()
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -119,7 +120,24 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+# STATIC_URL = 'static/'
+STATIC_URL = "/static/" 
+MEDIA_URL = '/images/'
+
+STATICFILES_DIRS = [  
+    os.path.join(
+        BASE_DIR, "static"
+    ),  
+]
+STATIC_ROOT = os.path.join(
+    BASE_DIR, "staticfiles"
+)  
+
+'''media files'''
+MEDIA_URL = "/media/"  
+MEDIA_ROOT = os.path.join(
+    BASE_DIR, "media"
+)  
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
