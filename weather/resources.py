bb41eca85d0f7c46865c2517f78ac0faf9ab54e8
Chetram Bassit <chetrambassit121@gmail.com>
2023-07-14 12:18:41 -0400

    created simpleupload function

--- weather/resources.py
+++ weather/resources.py
@@ -4,12 +4,3 @@ from .models import USCities
 class USCitiesResource(resources.ModelResource):
     class Meta:
         model = USCities
-        
-        
-        
-# from import_export import resources
-# from .models import Employee
-
-# class EmployeeResource(resources.ModelResource):
-#     class Meta:
-#         model = Employee
--- weather/views.py
+++ weather/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import USCities
+from .resources import USCitiesResource
+from django.contrib import messages
+from tablib import Dataset
 
-# Create your views here.
+
+'''  
+Upload a xlsx file to djangos database to create objects based on the model USCities 
+Package/Library Requirements: django-import-export
+Required Files: resources.py
+'''
+def simple_upload(request):
+    if request.method == 'POST':
+        cities_resource = USCitiesResource()
+        dataset = Dataset()
+        new_city = request.FILES['myfile']
+        
+        if not new_city.name.endswith('xlsx'):
+            messages.info(request,'wrong format')
+            return render(request, 'weather/upload.html')
+        
+        imported_data = dataset.load(new_city.read(), format='xlsx')
+        for data in imported_data:
+            value = USCities(data[0], data[1], data[2])
+            value.save()
+    return render(request, 'upload.html')
